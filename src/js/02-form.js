fda0260d5192762efa45d5530574f86995043906
Chercheuse <108093435+Chercheuse@users.noreply.github.com>
2024-01-11 22:43:25 +0400

    set up local storage for email and message in form

---
+++ src/js/02-form.js
@@ -0,0 +1,45 @@
+const form = document.querySelector('.feedback-form');
+const localStorageKey = 'feedback-form-state';
+
+form.addEventListener('input', updateLocalStorage);
+form.addEventListener('submit', handleSubmit);
+
+const storedData = JSON.parse(localStorage.getItem(localStorageKey)) || {};
+form.email.value = storedData.email || '';
+form.message.value = storedData.message || '';
+
+function updateLocalStorage() {
+  if (form.email.name === 'email' || form.message.name === 'message') {
+    localStorage.setItem(
+      localStorageKey,
+      JSON.stringify({
+        email: form.email.value.trim(),
+        message: form.message.value.trim(),
+      })
+    );
+  }
+}
+
+function handleSubmit(event) {
+  event.preventDefault();
+  if (areFieldsFilled()) {
+    console.log(getFormData());
+    clearLocalStorageAndForm();
+  }
+}
+
+function areFieldsFilled() {
+  return form.email.value.trim() !== '' && form.message.value.trim() !== '';
+}
+
+function getFormData() {
+  return {
+    email: form.email.value.trim(),
+    message: form.message.value.trim(),
+  };
+}
+
+function clearLocalStorageAndForm() {
+  localStorage.removeItem('feedback-form-state');
+  form.reset();
+}
